6b2cf8a71ed658dc877389c38f9c56e0ec060164
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-01-16 14:42:17 -0700

    Added a sample program

---
+++ FirstProgram/build/CMakeFiles/wxTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxTest.dir/myapp.cpp.obj"
+  "libwxTest.dll.a"
+  "wxTest.exe"
+  "wxTest.exe.manifest"
+  "wxTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wxTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
