3b3e1e3d8691bc3480f58892323dce9c91d38e24
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-02-25 00:32:07 -0700

    Added a button sample

--- Syntaxical/Buttons/include/Buttons.h
+++ Syntaxical/Buttons/include/Buttons.h
@@ -9,7 +9,15 @@ public:
 class Frame: public wxFrame
 { 
 public: 
-	Frame(const wxString &title, const wxPoint &pos, const wxSize &size); 
+	Frame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+	////
+	void OnExit(wxCommandEvent&);
+
+	wxButton* exitButton;
+
+	DECLARE_EVENT_TABLE()
+	////
 }; 
 
 DECLARE_APP(App)
--- Syntaxical/Buttons/src/Buttons.cpp
+++ Syntaxical/Buttons/src/Buttons.cpp
@@ -3,12 +3,18 @@
 
 IMPLEMENT_APP(App)
 
+////
+BEGIN_EVENT_TABLE(Frame, wxFrame)
+	EVT_BUTTON(wxID_EXIT, Frame::OnExit)
+END_EVENT_TABLE()
+////
+
 bool App::OnInit() 
 {
 	Frame *window = new Frame(
 		wxT("Hello World!"),
 		wxPoint(10, 10),
-		wxSize(800, 800)
+		wxSize(1200, 1000)
 	);
 
 	window->Show(true);
@@ -19,4 +25,21 @@ bool App::OnInit()
 Frame::Frame(const wxString& title, const wxPoint& pos, const wxSize& size) 
 : wxFrame(nullptr, wxID_ANY, title, pos, size) 
 {
+	////
+	exitButton = new wxButton(
+		this,
+		wxID_EXIT,
+		wxT("Exit Button"),
+		wxDefaultPosition,
+		wxSize(10, 5),
+		0
+	);
+	////
 }
+
+////
+void Frame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+////
